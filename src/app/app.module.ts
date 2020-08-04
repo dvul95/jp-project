4ea3b0537b86688f5eb21bdf321732e854b4d1ef
Dunja Vulovic <dunjavulovic95@gmail.com>
2020-08-04 14:34:35 +0200

    added and changed new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+
 import { HiraganaComponent } from './hiragana/hiragana.component';
+import { KatakanaComponent } from './katakana/katakana.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HiraganaComponent
+    AppComponent, HiraganaComponent, KatakanaComponent
   ],
   imports: [
     BrowserModule,
